b28f1abe89fc237d5373555acbee0b7037e8d83d
Irina Zakharova <letter.zakharova@ya.ru>
2021-12-27 00:20:43 +0300

    comment added

--- Composite/Composite.mjs
+++ Composite/Composite.mjs
@@ -1,3 +1,7 @@
+/**
+ * @description Паттерн Компоновщик, собирает разные классы внутри себя и работает с их методами.
+ *  У классов должен быть единый интерфейс
+ */
 export class Composite {
  constructor(arrOfComponents) {
   this.components = arrOfComponents
